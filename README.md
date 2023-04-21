36dc2c5ad6d0e59287686f92bce5fb4679afa2b4
amirvhd <65691404+amirvhd@users.noreply.github.com>
2023-04-21 19:26:30 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,68 @@
+
+# 
+
+This repository is for "Probabilistic Self-supervised Learning via
+Scoring Rules Minimization" paper. 
+
+
+
+## Table of contents
+* [Badges](#general-information)
+* [Installation](#Installation)
+* [Usage/Examples](#Usage/Examples)
+* [Acknowledgements](#Acknowledgements)
+
+## Badges
+
+[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt)
+
+### Dependency
+
+![Python](https://img.shields.io/badge/Python-3.9-brightgreen)
+![torch](https://img.shields.io/badge/Torch-1.10.1-brightgreen)
+
+## Installation
+
+Install requirments:
+```python
+pip install -r requirements.txt
+```
+
+
+## Usage/Examples
+
+
+### Pretraining
+
+You can run the pretraining of model for Imagenet with following code.
+
+```python
+torchrun --nproc_per_node=8 main_dino.py --arch vit_small 
+``` 
+### Linear evaluation
+
+You can run the linear-evaluation of the model for Imagenet with the following code. 
+
+```python
+torchrun --nproc_per_node=8 eval_linear.py --arch vit_small
+``` 
+
+### Uncertainty evaluation 
+
+```python
+python 
+```
+ 
+### Out of distribtuion detection
+
+
+```python
+python 
+```
+
+
+
+## Acknowledgements
+Base DINO adapted from following repository:
+
+ - [DINO](https://github.com/facebookresearch/dino)
---
+++ scoreloss.py
@@ -0,0 +1,72 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.distributed as dist
+import torch.nn.functional as F
+
+
+def gaussian_kernel(x1, x2, gamma=1):
+    a = -F.mse_loss(x1, x2, reduction='sum')
+    print(a.shape)
+    return torch.exp(a)
+
+
+class SCORELoss(nn.Module):
+    def __init__(self, out_dim, ncrops,
+                 center_momentum=0.85, l=2.4):
+        super().__init__()
+        self.center_momentum = center_momentum
+        self.ncrops = ncrops
+        self.register_buffer("center", torch.zeros(1, out_dim))
+        self.out = out_dim
+        self.n_sample = 4
+        self.dist = torch.distributions.Normal(loc=torch.zeros(self.out, device="cuda"),
+                                               scale=torch.ones(self.out, device="cuda")
+                                               )
+        self.l = l
+
+    def forward(self, mean_st, var_st, teacher_output, epoch):
+        self.batch = mean_st.shape[0]
+
+        samples = self.dist.rsample(sample_shape=torch.tensor([self.n_sample, self.batch]))
+        student_sample = (
+                (samples * var_st.unsqueeze(0)) + mean_st.unsqueeze(0))
+
+        # temp = self.teacher_temp_schedule[epoch]
+        # teacher_out = F.softmax((teacher_output - self.center) / temp, dim=-1)
+        teacher_output = (teacher_output - self.center)
+        teacher_out = teacher_output.detach().chunk(2)
+
+        student_sample_chunk = student_sample.chunk(self.ncrops, dim=1)
+        loss_second_part = 0
+        loss_first_part = 0
+        n_loss = 0
+
+        for i in range(self.n_sample):
+            for iq, q in enumerate(teacher_out):
+                # if iq < 3:
+                for iv in range(len(student_sample_chunk)):
+                    loss_first_part += 2 * F.l1_loss(student_sample_chunk[iv][i, :, :], q)
+                    n_loss += 1
+
+            if i != self.n_sample - 1:
+                for j in range(i + 1, self.n_sample):
+                    loss_second_part += - (1 / (self.n_sample - 1)) * F.l1_loss(student_sample[i, :, :],
+                                                                                student_sample[j, :, :])
+
+        total_loss = 10 * (
+                (loss_first_part + 0.000009) / n_loss + self.l * (loss_second_part + 0.000009) / self.n_sample)
+        self.update_center(teacher_output)
+        return total_loss, loss_first_part / n_loss, loss_second_part / self.n_sample
+
+    @torch.no_grad()
+    def update_center(self, teacher_output):
+        """
+        Update center used for teacher output.
+        """
+        batch_center = torch.sum(teacher_output, dim=0, keepdim=True)
+        dist.all_reduce(batch_center)
+        batch_center = batch_center / (len(teacher_output) * dist.get_world_size())
+
+        # ema update
+        self.center = self.center * self.center_momentum + batch_center * (1 - self.center_momentum)
