d9bdc549e40f522a127f56f368bf367cd4d732cf
amirvhd <65691404+amirvhd@users.noreply.github.com>
2023-05-23 22:11:35 +0200

    add new files

--- main_prosmin.py
+++ main_prosmin.py
@@ -1,17 +1,8 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-# /dss/dssmcmlfs01/pn69za/pn69za-dss-0002/mina/ILSVRC/Data/CLS-LOC/train
-# torchrun --nproc_per_node=8 main_dino.py --arch vit_small --data_path /dss/dssmcmlfs01/pn69za/pn69za-dss-0002/mina/ILSVRC/Data/CLS-LOC/train --output_dir ./s
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright (c) ByteDance, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
 """
 Some parts of this code is Copy-paste from DINO library:
 https://github.com/facebookresearch/dino
--- scoreloss.py
+++ scoreloss.py
@@ -1,4 +1,9 @@
-import numpy as np
+# Copyright (c) ByteDance, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 import torch
 import torch.nn as nn
 import torch.distributed as dist
